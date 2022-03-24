7a0934220e3942d92ace7d98f0d552f38baf31e7
SadFarm1 <76851356+SadFarm1@users.noreply.github.com>
2022-03-23 23:27:34 -0400

    customize ticks

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,74 +1,103 @@
 import discord
 from discord.ext import commands
-import datetime
-import time
+import os
+import requests
+from dotenv import load_dotenv, find_dotenv
 
-class Tickets(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+load_dotenv(find_dotenv())
+
+bot = commands.Bot(command_prefix='^')
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print('Loading cogs...')
+    load_cogs()
+    print('Done loading cogs')
+    print('------')
+
+    check_msg_file()
+
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+
+def check_cog(cog):
+    return os.path.isfile(f'cogs/{cog}.py')
+
+@bot.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    if(check_cog(extension)):
+        bot.load_extension(f'cogs.{extension}')
+        await ctx.send(f'Loaded module "{extension}"')
+    else:
+        await ctx.send(f'Could not find module "{extension}"')
+
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    if(check_cog(extension)):
+        bot.unload_extension(f'cogs.{extension}')
+        await ctx.send(f'Unloaded module "{extension}"')
+    else:
+        await ctx.send(f'Could not find module "{extension}"')
+
+
+@bot.command(aliases=['r'])
+@commands.check_any(commands.is_owner(), commands.has_permissions(kick_members=True))
+async def reload(ctx, extension):
+    if(check_cog(extension)):
+        wait_msg = await ctx.send(f'Reloading module: "{extension}"')
+        url = f'https://raw.githubusercontent.com/SadFarm1/happyhelper/master/cogs/{extension}.py'
+        r = requests.get(url)
+    
+        if(r.content != b'Not found.\n'):
+            open(f'./cogs/{extension}.py', 'wb').write(r.content)
+            
+        bot.reload_extension(f'cogs.{extension}')
+        await ctx.send(f'Reloaded module "{extension}"')
+        
+    else:  
+        await ctx.send(f'Could not find module "{extension}"')
+
+
+@bot.command()
+@commands.is_owner()
+async def download(ctx, extension):
+    wait_msg = await ctx.send(f'Downloading module: "{extension}"')
+    url = f'https://raw.githubusercontent.com/SadFarm1/happyhelper/master/cogs/{extension}.py'
+    r = requests.get(url)
+    
+    if(r.content != b'Not found.\n'):
+        open(f'./cogs/{extension}.py', 'wb').write(r.content)
+        await wait_msg.edit(content=f'Downloaded module "{extension}"')
+    else:
+        await wait_msg.edit(content=f'Could not find module "{extension}"')
+
+
+@download.error
+async def download_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify a module to download')
+    if(isinstance(error, commands.NotOwner)):
+        await ctx.send('You do not have permission to use this command')
+
+
+def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
+
+def check_msg_file():
+    if not os.path.exists('messages.json'):
+        os.close(os.open("file.txt", os.O_CREAT))
+        f =  open('messages.json', 'w')
+        f.write('{"presales": [], "support": []}')
+        f.close()
         
 
-    
-    
-    
-    @commands.Cog.listener()
-    async def on_guild_channel_create(self, channel):
-
-        time.sleep(2.5)
-        now = datetime.date.today()
-        day = now.strftime('%w')
-
-        now = datetime.datetime.today()
-        hour = int(now.strftime('%H'))
-
-
-
-        if int(hour) <= 15 or int(hour) >= 23 and int(day) != 6 and int(day) != 0:
-            if ('presales' in str(channel)):
-
-                embed = discord.Embed(
-                    title = "Presale Alert",
-                    description = "Thank you for opening a ticket! You have reached us after our regular business hours. Response times may be impacted. Please leave a detailed message so we can assist you as soon as possible! \n\nThank you! \nExpect a response at around 7 AM (PST)",
-                    color = 0x004c8b
-                )
-                await channel.send(embed=embed)
-
-            elif ('support' in str(channel)):
-                
-                embed = discord.Embed(
-                    title = "Support Alert",
-                    description = "Thank you for opening a ticket! You have reached us after our regular business hours. Response times may be impacted. Please try our self service commands and/or leave a detailed message so we can assist you as soon as possible! \n\nThank you! \nExpect a response at around 7 AM (PST)",
-                    color = 0x004c8b
-                )
-                await channel.send(embed=embed)
-        
-        elif int(day) == 6 or int(day) == 0 :
-
-            if ('presales' in str(channel)):
-
-                embed = discord.Embed(
-                    title = "Presale Alert",
-                    description = "Thank you for opening a Presales ticket! You have reached us during our weekend hours. Response times may be impacted. Please leave a detailed message so we can assist you as soon as possible!  \n\nThank you! \nExpected response times between 7 AM - 9 PM (PST)",
-                    color = 0x004c8b
-                )
-                await channel.send(embed=embed)
-
-            elif ('support' in str(channel)):
-
-                embed = discord.Embed(
-                    title = "Support Alert",
-                    description = "Thank you for opening a Support ticket! You have reached us during our weekend hours. Response times may be impacted. Please try our self service commands and/or Please leave a detailed message so we can assist you as soon as possible!   \n\nThank you! \nExpected response times between 7 AM - 9 PM (PST)",
-                    color = 0x004c8b
-                )
-                await channel.send(embed=embed)
-        
-
-
-
-
-
-def setup(bot):
-    bot.add_cog(Tickets(bot))
-
-
-    
+bot.run(os.getenv('DISCORD_KEY'))
